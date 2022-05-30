9109cf5386f7ee22615cfc09fe40416a1cb2e5b1
MarshmallowShadow <choiboseung@naver.com>
2022-05-30 14:01:15 +0900

    2022-05-30

--- phonedb.sql
+++ phonedb.sql
@@ -25,13 +25,13 @@ insert into person
 values (seq_person_id.nextval, '정우성', '010-2222-2222', '02-2222-2222');
 
 insert into person
-values (seq_person_id.nextval, '유재석', '010-3333-3333', '010-3333-3333');
+values (seq_person_id.nextval, '유재석', '010-3333-3333', '02-3333-3333');
 
 insert into person
-values (seq_person_id.nextval, '이정재', '010-4444-4444', '010-4444-4444');
+values (seq_person_id.nextval, '이정재', '010-4444-4444', '02-4444-4444');
 
 insert into person
-values (seq_person_id.nextval, '서장훈', '010-5555-5555', '010-5555-5555');
+values (seq_person_id.nextval, '서장훈', '010-5555-5555', '02-5555-5555');
 
 insert into person
 values (seq_person_id.nextval, 'Marsh', '010-2074-8889', '(없음)');
